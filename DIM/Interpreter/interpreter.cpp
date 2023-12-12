a6317e92406f98de4f6ef1b462fad01f4f77704b
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-12-12 11:36:07 +0300

    Update interpreter.cpp

--- DIM/Interpreter/interpreter.cpp
+++ DIM/Interpreter/interpreter.cpp
@@ -3,31 +3,28 @@
 #define DEBUG true
 
 #include "lexer.h"
-#include "parser.h"
+
 
 void PrintLexerResult(std::vector<Token> tokens) {
 	for (size_t i = 0; i < tokens.size(); ++i) {
-		std::cout << tokens[i].value << "-Type:" << tokens[i].type << " ";
+		std::cout << tokens[i].code << " ";
+		//std::cout << tokens[i].code << " ";
 		if (tokens[i].type == Type::TYPE_EOL) {
 			std::cout << '\n';
 		}
 	}
 }
 
+
 int main() {
 	std::setlocale(LC_ALL, "Ru");
-	std::string text = "out 6.0 != 7;\nout 5.0 >= 7;\nout 1.0 <= 7;\nout 7.0 == 7;\nout 5 + 6 - 5;\n~out \"Hello, World! 123\";\n~out \"Привет, Мир!\";\nvar a = 5;\nout a--;\nout a++;";
+	std::string text = "int a = 5;\nif((a >= 5 || a < 10) && a != 9){\nout \"Yes\";}\n";
 
 	Lexer lexer(text);
-	std::vector<Token> tokens = lexer.Lex();
+
+	std::vector<Token>tokens = lexer.Lex();
 	
 	if (DEBUG) { PrintLexerResult(tokens); }
 
-	Parser parser(tokens);
-	
-	std::cout << parser.Parse();
-	
-	
 	return 0;
 }
-
