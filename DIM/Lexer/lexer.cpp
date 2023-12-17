4bca7940f3039200eb869b5bf7e2bf06e28d5631
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2023-12-17 15:23:39 +0300

    Update lexer.cpp

--- DIM/Lexer/lexer.cpp
+++ DIM/Lexer/lexer.cpp
@@ -1,6 +1,5 @@
 #include "lexer.h"
 #include "error.h"
-#include "tokens.h"
 
 // Имплементация Advance
 void Lexer::Advance(int step) {
@@ -15,30 +14,41 @@ Token Lexer::GetString(int first_quote_position, int second_quote_position) {
 		temp_string += text_[first_quote_position];
 		Advance();
 	}
-	if(CODE_STRING_LITERALS.count(temp_string) == 0){
-		++string_literals_count_;
+	++string_literals_count_;
+	std::string code = "C" + std::to_string(string_literals_count_);
+	// Если нет такого литерала заносим в словарь и выводим
+	if (CODE_STRING_LITERALS.count(temp_string) == 0) {
 		structure_token.type = Type::TYPE_STRING;
-		structure_token.code = "C" + std::to_string(string_literals_count_);
+		structure_token.code = code;
+		structure_token.value = temp_string;
+		structure_token.token_position = position_ - text_.length();
+		CODE_STRING_LITERALS[temp_string] = "C" + std::to_string(string_literals_count_);
+		return structure_token;
+	}
+	// Если выводим
+	else {
+		structure_token.type = Type::TYPE_STRING;
+		structure_token.code = CODE_STRING_LITERALS[temp_string];
 		structure_token.value = temp_string;
 		structure_token.token_position = position_ - text_.length();
 		return structure_token;
 	}
-	
+
 }
 
 // Имплементация GetCommand
 Token Lexer::GetCommand() {
 	std::string temp_string = "";
-	
+
 	while (std::isalpha(current_char_)) {
 		temp_string += current_char_;
 		Advance();
 	}
-	
+
 	// Проверяем, является ли temp_string командой
 	bool is_registry_command = (std::find(begin(REGISTRY_COMMANDS_), end(REGISTRY_COMMANDS_), temp_string) != end(REGISTRY_COMMANDS_)) ? true : false;
-	
-	if (is_registry_command){
+
+	if (is_registry_command) {
 		if (temp_string == "or") {
 			structure_token.type = Type::TYPE_OR;
 			structure_token.code = BinaryLogicAndMathOperationCode_[temp_string];
@@ -58,20 +68,30 @@ Token Lexer::GetCommand() {
 		structure_token.value = temp_string;
 		structure_token.token_position = position_ - temp_string.length();
 		return structure_token;
-	}else {
+	}
+	else {
 		// Если temp_string нет в REGISTRY_COMMANDS_ вероятно это переменная
+		++varible_count_;
+		std::string code = "I" + std::to_string(varible_count_);
+		// Если нет такой переменной заносим в словарь и выводим
 		if (CODE_VARIBLES.count(temp_string) == 0) {
-			++varible_count_;
 			structure_token.type = Type::TYPE_VARIBLE;
-			structure_token.code = "I" + std::to_string(varible_count_);
+			structure_token.code = code;
 			structure_token.value = temp_string;
 			structure_token.token_position = position_ - temp_string.length();
-			CODE_VARIBLES[structure_token.code] = temp_string;
-			
+			CODE_VARIBLES[temp_string] = code;
+			return structure_token;
+		}
+		// Если есть выводим
+		else {
+			structure_token.type = Type::TYPE_VARIBLE;
+			structure_token.code = CODE_VARIBLES[temp_string];
+			structure_token.value = temp_string;
+			structure_token.token_position = position_ - temp_string.length();
 			return structure_token;
 		}
 	}
-} 
+}
 
 // Имплементация GetDigit
 Token Lexer::GetDigit() {
@@ -82,12 +102,27 @@ Token Lexer::GetDigit() {
 	}
 	// Проверяем, является ли temp_digit числом с плавающей точкой
 	bool is_float = (temp_digit.find(Separator_Tokens::DOT_TOKEN) != std::string::npos);
-
-	structure_token.type = is_float ? Type::TYPE_FLOAT : Type::TYPE_INT;
-	structure_token.code = temp_digit;
-	structure_token.value = temp_digit;
-	structure_token.token_position = position_ - temp_digit.length();
-	return structure_token;
+	++digit_count_;
+	std::string code = "N" + std::to_string(digit_count_);
+	// Если нет такого числа заносим в словарь и выводим
+	if(CODE_DIGITS.count(temp_digit) == 0){
+		structure_token.type = is_float ? Type::TYPE_FLOAT : Type::TYPE_INT;
+		structure_token.code = code;
+		structure_token.value = temp_digit;
+		structure_token.token_position = position_ - temp_digit.length();
+		CODE_DIGITS[temp_digit] = code;
+		return structure_token;
+	}
+	// Если есть выводим
+	else {
+		--digit_count_;
+		structure_token.type = is_float ? Type::TYPE_FLOAT : Type::TYPE_INT;
+		structure_token.code = CODE_DIGITS[temp_digit];
+		structure_token.value = temp_digit;
+		structure_token.token_position = position_ - temp_digit.length();
+		return structure_token;
+	}
+	
 }
 
 // Главный метод для анализа токенов
@@ -96,7 +131,7 @@ std::vector<Token> Lexer::Lex() {
 	int first_quote_position = -1, second_quote_position;
 
 	while (position_ < text_.length()) {
-		
+
 		// Пропустить пробелы
 		if (std::isspace(current_char_)) {
 			Advance();
@@ -106,7 +141,7 @@ std::vector<Token> Lexer::Lex() {
 		if (current_char_ == Separator_Tokens::QUOTE_TOKEN) { first_quote_position = position_; } // найти позицию "
 
 		// Получить >=
-		if (current_char_ == Logic_Operation_Tokens::GREATER_TOKEN && text_[position_+1] == Logic_Operation_Tokens::EQUAL_TOKEN) {
+		if (current_char_ == Logic_Operation_Tokens::GREATER_TOKEN && text_[position_ + 1] == Logic_Operation_Tokens::EQUAL_TOKEN) {
 			structure_token.type = Type::TYPE_GREATER_EQUAL;
 			structure_token.code = BinaryLogicAndMathOperationCode_[">="];
 			structure_token.value = ">=";
@@ -126,7 +161,7 @@ std::vector<Token> Lexer::Lex() {
 			Advance(2);
 			continue;
 		}
-		
+
 		// Получить ==
 		if (current_char_ == Logic_Operation_Tokens::EQUAL_TOKEN && text_[position_ + 1] == Logic_Operation_Tokens::EQUAL_TOKEN) {
 			structure_token.type = Type::TYPE_EQUAL_EQUAL;
@@ -198,7 +233,8 @@ std::vector<Token> Lexer::Lex() {
 			structure_token.value = current_char_;
 			structure_token.token_position = position_;
 			tokens.push_back(structure_token);
-		}else {
+		}
+		else {
 			// бросить ошибку о неизвестном токене
 			std::cout << UnknownCharacterError::RiseUnknownCharacterError("Неизвестный токен: " + std::to_string(current_char_) + "\nПозиция: " + std::to_string(structure_token.token_position)) << '\n';
 			exit(1);
