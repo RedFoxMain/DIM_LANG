b1efeed4067bb08db1f19dad70ddf0488d6ea282
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2024-01-01 20:03:21 +0300

    Update parser.h

--- DIM/Parser/parser.h
+++ DIM/Parser/parser.h
@@ -1,1 +1,14 @@
+#include <vector>
 
+#include "tokens.h"
+#include "opn.h"
+
+
+class Parser {
+public:
+	explicit Parser(std::vector<Token> data): hiddenData_(data) {} // Конструктор
+	~Parser(){} // Деструктор
+
+private:
+	std::vector<Token> hiddenData_; // Вектор закодированных ключевых слов DIM
+};
