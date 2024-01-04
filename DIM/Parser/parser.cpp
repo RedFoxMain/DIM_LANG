86609191906adc3b1e2e01ff3c51f9d39b4cddc4
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2024-01-04 20:41:43 +0300

    Update parser.cpp

--- DIM/Parser/parser.cpp
+++ DIM/Parser/parser.cpp
@@ -1,22 +1,43 @@
 #include <iostream>
 #include "parser.h"
 
-bool Parser::StartsWith(const std::string& text, std::string start) {
-	if (text.rfind(start)) { return true; }
-	return false;
+void Parser::GetStrings(std::map<std::string, std::string> &strings) {
+	for (const auto& it : strings) {
+		STRING_LITERALS_CODE_[it.second] = it.first;
+	}
+}
+
+void Parser::GetDigits(std::map<std::string, std::string>& digits) {
+	for (const auto& it : digits) {
+		DIGITS_CODE_[it.second] = it.first;
+	}
+}
+
+void Parser::GetVaribles(std::map<std::string, std::string>& varibles) {
+	for (const auto& it : varibles) {
+		VARIBLES_CODE_[it.second] = it.first;
+	}
+}
+
+void Parser::MakeFile(std::string data) {
+	std::ofstream out;
+	out.open("release.cpp");
+	if (out.is_open())
+	{
+		out << data;
+	}
+	out.close();
 }
 
 void Parser::TarnslateToCpp() {
-	
-	
-	std::string cpp_code = "";
+	std::string cpp_code = "#include <iostream>\n";
 	for (const auto& el: hiddenData_) {
-		if (StartsWith(el.code, "I")) { cpp_code += VARIBLES_CODE[el.code]; }
-		if (StartsWith(el.code, "C")) { cpp_code += STRING_LITERALS_CODE[el.code];}
-		if (StartsWith(el.code, "N")) { cpp_code += DIGITS_CODE[el.code];}
+		cpp_code += STRING_LITERALS_CODE_[el.code];
+		cpp_code += DIGITS_CODE_[el.code];
+		cpp_code += VARIBLES_CODE_[el.code];
 		cpp_code += OPN[el.code];
 		if (el.type == Type::TYPE_EOL || el.type == Type::TYPE_RFB || el.type == Type::TYPE_LFB) { cpp_code += "\n"; }
 		cpp_code += " ";
 	}
-	std::cout << cpp_code << std::endl;
+	MakeFile(cpp_code);
 }
