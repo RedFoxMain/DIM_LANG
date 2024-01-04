e0739ab5f89ac15f5042274153b144352e291871
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2024-01-04 20:41:55 +0300

    Update parser.h

--- DIM/Parser/parser.h
+++ DIM/Parser/parser.h
@@ -1,19 +1,23 @@
+#include <fstream>
+
 #include "tokens.h"
 #include "lexer.h"
 
 class Parser {
 public:
-	explicit Parser(std::vector<Token> data): hiddenData_(data) {} // Конструктор
+	explicit Parser(std::vector<Token> data): hiddenData_(data)  {} // Конструктор
 	~Parser(){} // Деструктор
 	void TarnslateToCpp(); // первести в cpp код
-	bool StartsWith(const std::string& text, std::string start);
-	std::map<std::string, std::string> VARIBLES_CODE; // получить переменную по коду
-	std::map<std::string, std::string> STRING_LITERALS_CODE; // получить строку по коду
-	std::map<std::string, std::string> DIGITS_CODE; // получить цифру по коду
-
+	void GetStrings(std::map<std::string, std::string> &strings); // Получить словарь строк
+	void GetDigits(std::map<std::string, std::string>& digits); // получить словарь цифр
+	void GetVaribles(std::map<std::string, std::string>& varibles); // Получить переменные
+	void MakeFile(std::string data); // Создать .cpp файл 
+	
 private:
 	std::vector<Token> hiddenData_; // Вектор закодированных ключевых слов DIM
-
+	std::map<std::string, std::string> STRING_LITERALS_CODE_; 
+	std::map<std::string, std::string> VARIBLES_CODE_;
+	std::map<std::string, std::string> DIGITS_CODE_;
 	// (ОПН) Обратная Польская Нотация
 	std::map<std::string, std::string> OPN = {
 	{"W1", "printf"}, {"W2", "getline"}, {"W3", "if"},
