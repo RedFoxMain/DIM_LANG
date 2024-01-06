cc5a8f10d5380b8f589193034eca8cb271e09f5c
RedFoxMain <132358716+RedFoxMain@users.noreply.github.com>
2024-01-06 20:33:05 +0300

    Update parser.h

--- DIM/Parser/parser.h
+++ DIM/Parser/parser.h
@@ -11,7 +11,8 @@ public:
 	void GetStrings(std::map<std::string, std::string> &strings); // Получить словарь строк
 	void GetDigits(std::map<std::string, std::string>& digits); // получить словарь цифр
 	void GetVaribles(std::map<std::string, std::string>& varibles); // Получить переменные
-	void MakeFile(std::string data); // Создать .cpp файл 
+	bool InVector(std::vector<std::string> list, std::string& word);
+	void MakeFile(std::string& data); // Создать .cpp файл 
 	
 private:
 	std::vector<Token> hiddenData_; // Вектор закодированных ключевых слов DIM
@@ -23,13 +24,12 @@ private:
 	{"W1", "printf"}, {"W2", "getline"}, {"W3", "if"},
 	{"W4", "else"},   {"W5", "while"},   {"W6", "for"},
 	{"W7", "size()"}, {"W8", "auto"},  {"W9", "void"},
-	{"W8", "auto"}, {"W9", "null", }, {"W10", "main"},
-	{"S1", "{"}, {"S2", "}"}, {"S3", "("}, {"S4", ")"},
-	{"S5", ";"}, {"S6", "\n"}, {"S7", "\t"}, {"S8", "["},
-	{"S9", "]"}, {"S10", "//"}, {"O1", "+"}, {"O2", "-"},
-	{"O3", "/"}, {"O4", "*"}, {"O5", "%"}, {"L1", ">"},
-	{"L2", "<"}, {"L3", "="}, {"L4", ">="}, {"L5", "<="},
-	{"L6", "=="}, {"L7", "!="}, {"L8", "||"}, {"L9", "&&"},
-	{"O6", "++"}, {"O7", "--"}
+	{"W10", "main"}, {"S1", "{"}, {"S2", "}"}, 
+	{"S3", "("}, {"S4", ")"}, {"S5", ";"}, {"S6", "\n"}, 
+	{"S7", "\t"}, {"S8", "["}, {"S9", "]"}, {"S10", "//"}, 
+	{"O1", "+"}, {"O2", "-"}, {"O3", "/"}, {"O4", "*"}, 
+	{"O5", "%"}, {"L1", ">"}, {"L2", "<"}, {"L3", "="}, 
+	{"L4", ">="}, {"L5", "<="}, {"L6", "=="}, {"L7", "!="}, 
+	{"L8", "||"}, {"L9", "&&"}, {"O6", "++"}, {"O7", "--"}
 	};
 };
